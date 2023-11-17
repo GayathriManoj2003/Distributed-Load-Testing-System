3d4481c9f1fd20155da687d92faf0cd3b2003d50
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2023-11-17 20:08:01 +0530

    Add Function to Handle 'test_config' Topic
    Modified handler for API endpoint /ping to accomodate change of RequestBody.
    Added function to create kafka producer for test_config topic.

--- server/orchestrator/main.go
+++ server/orchestrator/main.go
@@ -3,15 +3,30 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"net/http"
 	"crypto/rand"
 	"encoding/hex"
+	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
 type RequestBody struct {
-	NumberNodes      int    `json:"NumberNodes"`
 	TestType         string `json:"TestType"`
 	TestMessageDelay int    `json:"TestMessageDelay"`
+	NumRequests      int    `json:"NumRequests"`
+}
+
+type TestConfig struct {
+	TestID         string `json:"TestID"`
+	TestType         string `json:"TestType"`
+	TestMessageDelay int    `json:"TestMessageDelay"`
+	NumRequests      int    `json:"NumRequests"`
+}
+
+type Producer struct {
+	producer          *kafka.Producer
+	topic             string
+	deliveryChannel   chan kafka.Event
 }
 
 func generateUniqueToken() (string, error) {
@@ -24,9 +39,53 @@ func generateUniqueToken() (string, error) {
 	return token, nil
 }
 
+func CreateProducer(p *kafka.Producer, topic string) *Producer {
+	return &Producer{
+		producer:         p,
+		topic:            topic,
+		deliveryChannel:  make(chan kafka.Event, 10000),
+	}
+}
+
+func sendTestConfig(testConfiguration TestConfig) error {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         "something",
+		"acks":              "all",
+	})
+
+	if err != nil {
+		fmt.Printf("Failed to create producer: %s\n", err)
+		os.Exit(1)
+	}
+
+	prod := CreateProducer(p, "test_config")
+
+	jsonMessage, err := json.Marshal(testConfiguration)
+	if err != nil {
+		return err
+	}
+
+	err = prod.producer.Produce(&kafka.Message{
+			TopicPartition: kafka.TopicPartition{
+				Topic:     &prod.topic,
+				Partition: kafka.PartitionAny,
+			},
+			Value: jsonMessage,
+		}, prod.deliveryChannel)
+
+	if err != nil {
+		return err
+	}
+	<-prod.deliveryChannel
+
+	fmt.Printf("Placed message on the queue: %s\n", jsonMessage)
+	return nil
+}
+
 
 func main() {
-	http.HandleFunc("/test_config", handlePostTest)
+	http.HandleFunc("/ping", handlePostTest)
 	fmt.Println("Starting server on :8080...")
 	http.ListenAndServe(":8080", addCorsHeaders(http.DefaultServeMux))
 }
@@ -52,6 +111,18 @@ func handlePostTest(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Printf("Received POST request with body: %+v\n", requestBody)
 	fmt.Println("Test_ID:", Test_ID)
+
+	testConfig := TestConfig{
+		TestID:           Test_ID,
+		TestType:         requestBody.TestType,
+		TestMessageDelay: requestBody.TestMessageDelay,
+		NumRequests:      requestBody.NumRequests,
+	}
+	
+	fmt.Printf("Test Config: %+v\n", testConfig)
+
+	sendTestConfig(testConfig)
+	
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("POST request received successfully"))
 }
