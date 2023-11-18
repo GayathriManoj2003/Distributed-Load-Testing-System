f8c335d5a87cfbe783353aec97596a85a7d24e7c
shreya241103 <shreya241103@gmail.com>
2023-11-18 18:27:16 +0530

    added metric producer

--- server/driver/main.go
+++ server/driver/main.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"net/http"
 	"os"
-	"sync"
+	"os/signal"
+	"sort"
+	"syscall"
 	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 )
 
-// TestConfig represents the configuration for a test.
 type TestConfig struct {
 	TestID                string `json:"test_id"`
 	TestType              string `json:"test_type"`
@@ -19,135 +20,271 @@ type TestConfig struct {
 	MessageCountPerDriver int    `json:"message_count_per_driver"`
 }
 
-// TriggerMessage represents a trigger message for a test.
 type TriggerMessage struct {
 	TestID  string `json:"test_id"`
 	Trigger string `json:"trigger"`
 }
+type MetricMessage struct {
+	NodeID   string      `json:"node_id"`
+	TestID   string      `json:"test_id"`
+	ReportID string      `json:"report_id"`
+	Metrics  MetricsData `json:"metrics"`
+}
 
-// makeRequest performs an HTTP GET request to the given URL.
-func makeRequest(url string, wg *sync.WaitGroup) {
-	defer wg.Done()
+// MetricsData structure
+type MetricsData struct {
+	MeanLatency   int `json:"mean_latency"`
+	MedianLatency int `json:"median_latency"`
+	MinLatency    int `json:"min_latency"`
+	MaxLatency    int `json:"max_latency"`
+}
+type MetricProducer struct {
+	producer        *kafka.Producer
+	deliveryChannel chan kafka.Event
+}
 
-	resp, err := http.Get(url)
+func NewMetricProducer() *MetricProducer {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{
+		"bootstrap.servers": "localhost:9092",
+		"client.id":         "metric-producer",
+	})
+
 	if err != nil {
-		fmt.Printf("Error making request: %s\n", err)
+		fmt.Printf("Failed to create metric producer: %s\n", err)
+		os.Exit(1)
+	}
+
+	return &MetricProducer{
+		producer:        p,
+		deliveryChannel: make(chan kafka.Event, 10000),
+	}
+}
+
+// Close closes the MetricProducer
+func (mp *MetricProducer) Close() {
+	mp.producer.Close()
+}
+
+// PublishMetric sends a metric message to the metric_topic
+func (mp *MetricProducer) PublishMetric(metricMessage MetricMessage) {
+	jsonMessage, err := json.Marshal(metricMessage)
+	if err != nil {
+		fmt.Printf("Error encoding metric message: %v\n", err)
 		return
 	}
-	defer resp.Body.Close()
 
-	fmt.Printf("Status Code: %d\n", resp.StatusCode)
-}
+	topic := "metric_topic"
+	err = mp.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{
+			Topic:     &topic,
+			Partition: kafka.PartitionAny,
+		},
+		Value: jsonMessage,
+	}, mp.deliveryChannel)
 
-// startLoadTest initiates an HTTP load test for the given URL and number of concurrent requests.
-func startLoadTest(url string, numRequests int) {
-	var wg sync.WaitGroup
-	startTime := time.Now()
-
-	for i := 0; i < numRequests; i++ {
-		wg.Add(1)
-		go makeRequest(url, &wg)
+	if err != nil {
+		fmt.Printf("Failed to produce metric message: %v\n", err)
+		return
 	}
+	<-mp.deliveryChannel
 
-	wg.Wait()
-
-	duration := time.Since(startTime)
-	fmt.Printf("Total time taken for load test: %s\n", duration)
+	fmt.Printf("Published metric message:\n%s\n", jsonMessage)
 }
-
-// consumeTestConfigMessages subscribes to the specified Kafka topic and processes TestConfig messages.
-func consumeTestConfigMessages(consumer *kafka.Consumer, topic string) {
-	for {
-		ev := consumer.Poll(100)
-		switch e := ev.(type) {
-		case *kafka.Message:
-			var testConfig TestConfig
-			err := json.Unmarshal(e.Value, &testConfig)
-			if err != nil {
-				fmt.Printf("Error decoding JSON: %v\n", err)
-				continue
-			}
-
-			fmt.Printf("Received TestConfig: %+v\n", testConfig)
-			// Optionally, you can store the TestConfig or perform other actions
-
-		case kafka.Error:
-			fmt.Printf("Kafka error: %v\n", e)
-		}
-	}
-}
-
-// consumeTriggerMessages subscribes to the specified Kafka topic and processes TriggerMessage messages.
-func consumeTriggerMessages(consumer *kafka.Consumer, topic string) {
-	for {
-		ev := consumer.Poll(100)
-		switch e := ev.(type) {
-		case *kafka.Message:
-			var triggerMsg TriggerMessage
-			err := json.Unmarshal(e.Value, &triggerMsg)
-			if err != nil {
-				fmt.Printf("Error decoding trigger message: %s\n", err)
-				continue
-			}
-
-			if triggerMsg.Trigger == "YES" {
-				fmt.Printf("Received trigger message. Starting load test for TestID: %s\n", triggerMsg.TestID)
-
-				// Replace the URL with the actual URL for your load test
-				url := "https://api.github.com/"
-				numRequests := 10 // Number of concurrent requests
-
-				startLoadTest(url, numRequests)
-			}
-
-		case *kafka.Error:
-			fmt.Printf("Kafka consumer error: %v\n", e)
-		}
-	}
-}
-
-// main function for handling test configuration and trigger messages.
 func main() {
-	testConfigTopic := "test_config"
-	triggerTopic := "test_trigger"
+	topics := []string{"test_config", "trigger"}
 
-	consumerConfig := &kafka.ConfigMap{
+	consumer, err := kafka.NewConsumer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
-		"group.id":          "group1",
+		"group.id":          "my-group",
 		"auto.offset.reset": "earliest",
-	}
+	})
 
-	testConfigConsumer, err := kafka.NewConsumer(consumerConfig)
 	if err != nil {
-		fmt.Printf("Failed to create consumer for test config: %s\n", err)
+		fmt.Printf("Failed to create consumer: %s\n", err)
 		os.Exit(1)
 	}
 
-	triggerConsumer, err := kafka.NewConsumer(consumerConfig)
+	err = consumer.SubscribeTopics(topics, nil)
 	if err != nil {
-		fmt.Printf("Failed to create consumer for trigger messages: %s\n", err)
+		fmt.Printf("Failed to subscribe to topics: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Subscribe to test config and trigger topics
-	err = testConfigConsumer.SubscribeTopics([]string{testConfigTopic}, nil)
-	if err != nil {
-		fmt.Printf("Failed to subscribe to topic %s: %s\n", testConfigTopic, err)
-		os.Exit(1)
+	run := true
+	sigchan := make(chan os.Signal, 1)
+	signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM)
+	var count int
+	for run == true {
+		select {
+		case sig := <-sigchan:
+			fmt.Printf("Caught signal %v: terminating\n", sig)
+			run = false
+		default:
+			ev := consumer.Poll(100)
+			switch e := ev.(type) {
+			case *kafka.Message:
+				fmt.Printf("Received message on topic %s: %s\n", *e.TopicPartition.Topic, string(e.Value))
+
+				switch *e.TopicPartition.Topic {
+				case "test_config":
+					var testConfig TestConfig
+					// count := testConfig.MessageCountPerDriver
+					if err := json.Unmarshal(e.Value, &testConfig); err == nil {
+						storeTestConfig(testConfig)
+						count = testConfig.MessageCountPerDriver
+					}
+				case "trigger":
+					var triggerMessage TriggerMessage
+					if err := json.Unmarshal(e.Value, &triggerMessage); err == nil {
+						testID := triggerMessage.TestID
+						testConfig := retrieveTestConfig(consumer, testID, count)
+						performHTTPTest(testConfig)
+					}
+				}
+			case kafka.Error:
+				fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
+				run = false
+			default:
+				// Ignore other events
+			}
+		}
 	}
 
-	err = triggerConsumer.Subscribe([]string{triggerTopic}, nil)
-	if err != nil {
-		fmt.Printf("Failed to subscribe to topic %s: %s\n", triggerTopic, err)
-		os.Exit(1)
+	fmt.Println("Closing consumer")
+	consumer.Close()
+}
+
+// Helper function to store test configurations
+func storeTestConfig(testConfig TestConfig) {
+	// Implement logic to store the test configuration
+	fmt.Printf("Storing test configuration: %+v\n", testConfig)
+}
+
+// Helper function to retrieve test configuration for a given TestID
+func retrieveTestConfig(consumer *kafka.Consumer, testID string, count int) TestConfig {
+	// Implement logic to retrieve the test configuration
+	// This might involve polling for messages with the specified TestID
+	// and decoding the JSON content
+	// For simplicity, this function returns a dummy TestConfig
+	return TestConfig{
+		TestID:                testID,
+		TestType:              "Avalanche",
+		TestMessageDelay:      0,
+		MessageCountPerDriver: count,
+	}
+}
+
+// Helper function to perform HTTP tests and send metrics at a fixed interval
+func performHTTPTest(testConfig TestConfig) {
+	// Example: Report metrics every 5 seconds
+	// interval := 0.01 * time.Second
+	interval := 10 * time.Millisecond
+
+	fmt.Printf("Performing HTTP test for TestID: %s\n", testConfig.TestID)
+
+	url := "https://github.com"
+	numRequests := testConfig.MessageCountPerDriver
+	fmt.Printf("%d requests will be sent\n", numRequests)
+
+	var allLatencies []int     // Store all latencies for calculating cumulative metrics
+	var currentLatencies []int // Store latencies for the current interval
+
+	// Create metric producer
+	metricProducer := NewMetricProducer()
+
+	// Use a goroutine to periodically send metrics
+	go func() {
+		for {
+			time.Sleep(interval)
+
+			if len(currentLatencies) > 0 {
+				allLatencies = append(allLatencies, currentLatencies...)
+
+				meanLatency, medianLatency, minLatency, maxLatency := calculateLatencyMetrics(allLatencies)
+
+				// Create and publish metric message
+				metricMessage := MetricMessage{
+					NodeID:   testConfig.TestID,
+					TestID:   testConfig.TestID,
+					ReportID: testConfig.TestID,
+					Metrics: MetricsData{
+						MeanLatency:   meanLatency,
+						MedianLatency: medianLatency,
+						MinLatency:    minLatency,
+						MaxLatency:    maxLatency,
+					},
+				}
+				metricProducer.PublishMetric(metricMessage)
+
+				// Clear currentLatencies for the next interval
+				currentLatencies = nil
+			}
+		}
+	}()
+
+	// Perform HTTP requests
+	for i := 1; i <= numRequests; i++ {
+		startTime := time.Now()
+
+		// Send HTTP GET request
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Printf("Error sending HTTP request: %v\n", err)
+			return
+		}
+		defer resp.Body.Close()
+
+		// Measure latency
+		latency := int(time.Since(startTime).Milliseconds())
+
+		// Print results
+		fmt.Printf("Request %d - Latency: %d ms\n", i, latency)
+		currentLatencies = append(currentLatencies, latency)
 	}
 
-	fmt.Printf("Subscribed to topics: %s, %s\n", testConfigTopic, triggerTopic)
+	// Close the MetricProducer when done
+	metricProducer.Close()
 
-	// Start goroutines to consume messages for test config and trigger
-	go consumeTestConfigMessages(testConfigConsumer, testConfigTopic)
-	go consumeTriggerMessages(triggerConsumer, triggerTopic)
+	fmt.Printf("HTTP test completed for TestID: %s\n", testConfig.TestID)
+}
 
-	// Keep the main thread running
-	select {}
+func calculateLatencyMetrics(latencies []int) (int, int, int, int) {
+	// Calculate mean latency
+	var sumLatency int
+	for _, latency := range latencies {
+		sumLatency += latency
+	}
+	meanLatency := sumLatency / len(latencies)
+
+	// Calculate median latency
+	medianLatency := calculateMedian(latencies)
+
+	// Calculate min latency
+	sort.Ints(latencies)
+	minLatency := latencies[0]
+
+	// Calculate max latency
+	maxLatency := latencies[len(latencies)-1]
+
+	return meanLatency, medianLatency, minLatency, maxLatency
+}
+
+// Helper function to calculate the median of a slice of integers
+func calculateMedian(latencies []int) int {
+	n := len(latencies)
+	if n == 0 {
+		return 0
+	}
+
+	sort.Ints(latencies)
+
+	if n%2 == 0 {
+		// If the number of latencies is even, calculate the average of the two middle values
+		middle1 := latencies[n/2-1]
+		middle2 := latencies[n/2]
+		return (middle1 + middle2) / 2
+	} else {
+		// If the number of latencies is odd, return the middle value
+		return latencies[n/2]
+	}
 }
