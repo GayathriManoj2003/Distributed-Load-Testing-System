39481812653f1b2920471bd83d24cd97307b88fc
Gayathri Manoj <gayathrimanoj2103@gmail.com>
2023-11-19 20:24:36 +0530

    Remove React Strict Mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,9 +7,9 @@ import { TestIDProvider } from './context/TestIDContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <TestIDProvider>
-    <React.StrictMode>
+    {/* <React.StrictMode> */}
       <App />
-    </React.StrictMode>
+    {/* </React.StrictMode> */}
   </TestIDProvider>
 );
 
